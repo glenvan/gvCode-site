b8a9fc6c039dd5ba1ee1ea12069696aed72dbe9d
Glen van de Mosselaer <glenvan@gmail.com>
2011-03-05 15:49:43 -0700

    New colour scheme, removed obtrusive javascript.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
 $(document).ready(function(){
-	$('#site-name').animate({bottom: '0.5em'}, 1000, 'swing');
+	//$('#site-name').animate({bottom: '0.5em'}, 1000, 'swing');
+	//$('#header').delay(500).slideDown(1000, 'swing');
 });
