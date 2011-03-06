c1c56f58527f5afacbca94b79ad2b316a13fb895
Glen van de Mosselaer <glenvan@gmail.com>
2011-03-05 17:02:32 -0700

    More Layout. Expanding sections for apps.

--- js/index.js
+++ js/index.js
@@ -2,4 +2,8 @@
 $(document).ready(function(){
 	//$('#site-name').animate({bottom: '0.5em'}, 1000, 'swing');
 	//$('#header').delay(500).slideDown(1000, 'swing');
+	$('#pac-icon, #pac-header').click(
+		function(){
+			$('#pac-info').slideToggle(300, 'swing');
+		});
 });
