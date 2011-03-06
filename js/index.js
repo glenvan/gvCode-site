944c11d57466bfa94e760665173e738b6f862e89
Glen van de Mosselaer <glenvan@gmail.com>
2011-03-05 22:46:55 -0700

    Added a FancyBox gallery and thumbnails.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 	$('.product-info').hide();
 	
-	$('#pac-icon, #pac-header').click(
-		function(){
+	$('#pac-icon, #pac-header').click(function(){
 			$('#pac-info').slideToggle(300, 'swing');
+	});
+	
+	$('a.pac-screens').fancybox({'padding': 0, 'showNavArrows': true});
+	
+	$('p, .product-section').hover(
+		function(){
+			$(this).toggleClass('glowy');
+		},
+		function(){
+			$(this).removeClass('glowy');
 		});
 });
